1a91e05cc5e9b3f1add37fed6dc610d7362cbf26
Elvis Da Silva <129239900+3lvisDev@users.noreply.github.com>
2023-04-06 13:58:05 -0400

    Actualizacion de Clase 1

---
+++ Clase 1/Fahrenheit.py
@@ -0,0 +1,8 @@
+# Ingresamos la temperatura en grados Fahrenheit
+F = float(input("Ingresa la temperatura en grados Fahrenheit: "))
+
+# Convertimos la temperatura a grados Celsius usando la fórmula C = (F - 32) * (5/9)
+C = (F - 32) * (5/9)
+
+# Mostramos el resultado al usuario
+print("La temperatura en grados Celsius es:", C)
